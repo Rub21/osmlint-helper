48b274329aeb85f95e41fd2e942ab6b47bd5441f
Rub21 <ruben@mapbox.com>
2016-09-19 12:13:48 -0500

    add featureinline

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 'use strict';
 
 var program = require('commander');
+var argv = require('minimist')(process.argv.slice(2));
 var datateam = require('./src/datateam');
 var lastday = require('./src/lastday');
 var togeojson = require('./src/togeojson');
 var merge = require('./src/merge');
+var featureinline = require('./src/featureinline');
+
 program
   .version('0.0.1')
   .option('-y, --yesterday', 'filter yesterday changes')
@@ -14,6 +17,7 @@ program
   .option('-s, --splitperuser', 'Split json file into small geojson files')
   .option('-g, --togeojson', 'Convert osmlint output to geojson')
   .option('-m, --merge', 'merger all chunk ways in one in a geojson')
+  .option('-l, --featureinline', 'Set up each feature in line')
 
 .parse(process.argv);
 
@@ -32,4 +36,8 @@ if (program.togeojson) {
 }
 if (program.merge) {
   merge.merge(file);
+}
+if (program.featureinline) {
+  //type mean, the type of features that we will set in each line
+  featureinline.toline(file, argv.type);
 }
---
+++ src/featureinline.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var readline = require('readline');
+
+module.exports = {
+  toline: function(file, type) {
+    var types = ['Point', 'MultiPoint', 'LineString', 'MultiLineString', 'Polygon', 'MultiPolygon'];
+    if (type) {
+      types = type.split(',');
+    }
+    var rd = readline.createInterface({
+      input: fs.createReadStream(file),
+      output: process.stdout,
+      terminal: false
+    });
+    rd.on('line', function(line) {
+      var obj = JSON.parse(line);
+      obj.features.forEach(function(val) {
+        if (types.indexOf(val.geometry.type) > -1) {
+          process.stdout.write(JSON.stringify(val) + '\n');
+        }
+      });
+    }).on('close', function() {});
+  }
+};
